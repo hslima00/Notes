11a0584ff237bdb08f5d76289a03379686b6eca9
Hugo Lima <hslima00@gmail.com>
2022-09-07 04:07:16 +0100

    Deployed dca8771 with MkDocs version: 1.3.1

--- assets/javascripts/mathjax.js
+++ assets/javascripts/mathjax.js
@@ -1,25 +1,16 @@
 window.MathJax = {
-    tex: {
-      inlineMath: [["\\(", "\\)"]],
-      displayMath: [["\\[", "\\]"]],
-      processEscapes: true,
-      processEnvironments: true,
-      require: {
-        allow: {
-          base: false,
-          'all-packages': false
-        },
-        defaultAllow: true
-      }
-    },
-    options: {
-      ignoreHtmlClass: ".*|",
-      processHtmlClass: "arithmatex"
-    },
-    
-  };
-  
-  document$.subscribe(() => { 
-    MathJax.typesetPromise()
-  })
-  
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => { 
+  MathJax.typesetPromise()
+})
