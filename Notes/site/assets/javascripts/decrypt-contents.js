fc724fdd8e8b6ef28e16cd6d9891f8e558bd6bc3
Hugo Lima <hslima00@gmail.com>
2025-03-17 00:41:48 +0000

    tasks_json_update

--- Notes/site/assets/javascripts/decrypt-contents.js
+++ Notes/site/assets/javascripts/decrypt-contents.js
@@ -236,7 +236,8 @@ function decryptor_reaction(key_or_keys, password_input, decrypted_content, fall
         }
 
         // continue to decrypt others parts
-        
+        let encrypted_something = {'md-footer__inner': ['nav', 'class'], 'md-nav': ['nav', 'class']};
+        decrypt_somethings(key, encrypted_something);
         if (typeof inject_something !== 'undefined') {
             decrypted_content = decrypt_somethings(key, inject_something);
         }
